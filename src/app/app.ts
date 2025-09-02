dcb344ecf9bfd4487a220e60d9e5c0c5bfbef9f2
Liliia00_kh <Liliia00khimiak@gmail.com>
2025-09-02 12:01:39 +0300

    delete unneccessary file

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { MyApiService } from './my-api.service';
-import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { ProductListComponent } from './features/product/product-list.component/product-list.component';
 
 @Component({
